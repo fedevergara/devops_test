626da753e9f167a8194679cd991cfeb464004029
Federico Vergara Gómez <34877467+fedevergara@users.noreply.github.com>
2023-07-31 14:32:22 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,7 +5,7 @@ from desoper import devops_test
 class Test_hello(unittest.TestCase):
     def test__working(self):
         self.assertEqual(devops_test.hello(),
-                         'Hi, World!', True)
+                         'Hello, World!', True)
 
 
 if __name__ == '__main__':
